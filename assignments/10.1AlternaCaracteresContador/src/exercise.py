efff8e2c1b62133c2185d2d6f98fbaad861dfda5
Daviddiaz22 <88851000+Daviddiaz22@users.noreply.github.com>
2021-09-06 17:34:40 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    for i in range(a,b+1,1):
+        if i%2 == 0:
+            print(str(i)+"-%")
+        else:
+            print(str(i)+"-#")
 
 if __name__=='__main__':   
     main()
